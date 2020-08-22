f749843f330a4eafdda37d247358ea55092ec3f6
Nate <nate@pop-os.localdomain>
2020-08-22 17:45:06 +0100

    Updates

--- components/bio/intro.js
+++ components/bio/intro.js
@@ -1,33 +1,21 @@
-import _slicedToArray from "@babel/runtime/helpers/slicedToArray";
 import React, { useState } from 'react';
 import { useSpring, animated as a } from "react-spring";
 import man from "../../images/man.png";
 import { set } from 'react-ga';
 
 function Intro() {
-  var _useState = useState(false),
-      _useState2 = _slicedToArray(_useState, 2),
-      on = _useState2[0],
-      set = _useState2[1];
-
-  var props = useSpring({
-    opacity: on ? 1 : 0
-  });
-
-  var change = function change(e) {
-    var elem = e.target;
-
-    if (elem.scrollHeight - elem.scrollTop === elem.clientHeight) {
-      set(!on);
-    }
-  };
-
+  // const [on, set] = useState(false);
+  // const props = useSpring({opacity: on ? 1 : 0})
+  // const change = (e) => {
+  //     let elem = e.target;
+  //     if ( elem.scrollHeight - elem.scrollTop === elem.clientHeight) {
+  //         set(!on);
+  //     }
+  // }
   return /*#__PURE__*/React.createElement("div", {
     className: "profile",
     onScroll: change
-  }, /*#__PURE__*/React.createElement("button", {
-    onClick: change
-  }, "button"), /*#__PURE__*/React.createElement(a.div, {
+  }, /*#__PURE__*/React.createElement(a.div, {
     className: "bio",
     style: props
   }, /*#__PURE__*/React.createElement("img", {
